008ca338824a4cd7099bb8ad3f7a2cc71701a52b
cristian esem <cristian.esem@softtek.com>
2020-11-22 17:56:38 -0300

    feat: post y get para un solo usuario

--- index.js
+++ index.js
@@ -1,14 +1,91 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+let usuario = {
+ usuario:'',
+ nivel: '',
+ pavos: '',
+ region: '',
+};
+let respuesta = {
+ error: false,
+ codigo: 200,
+ mensaje: ''
+};
+app.get('/', function(req, res) {
+ respuesta = {
+  error: true,
+  codigo: 200,
+  mensaje: 'Punto de inicio'
+ };
+ res.send(respuesta);
+});
+app.get('/usuario', function (req, res) {
+ respuesta = {
+  error: false,
+  codigo: 200,
+  mensaje: ''
+ };
+ if(usuario.usuario !== '' || usuario.nivel !== '' || usuario.pavos !== '' || usuario.region !== '') {
+  respuesta = {
+   error: true,
+   codigo: 501,
+   mensaje: 'El usuario no ha sido creado'
+  };
+ } else {
+  respuesta = {
+   error: false,
+   codigo: 200,
+   mensaje: 'respuesta del usuario',
+   respuesta: usuario
+  };
+ }
+ res.send(respuesta);
+});
+app.post('/usuario', function (req, res) {
+ if(!req.body.usuario  || !req.body.nivel || !req.body.pavos || !req.body.region) {
+  respuesta = {
+   error: true,
+   codigo: 502,
+   mensaje: 'todos los campos son requeridos'
+  };
+ } else {
+  if(usuario.usuario !== '' || usuario.nivel !== '' || usuario.pavos !== '' || usuario.region !== '') {
+   respuesta = {
+    error: true,
+    codigo: 503,
+    mensaje: 'El usuario ya fue creado previamente'
+   };
+  } else {
+   usuario = {
+    usuario: req.body.usuario,
+    pavos: req.body.pavos,
+    region: req.body.region,
+    nivel: req.body.nivel
 
-app.get('/', function (req, res) {
-    res.send('Saludos desde express');
-  }); 
+   };
+   respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: 'Usuario creado',
+    respuesta: usuario
+   };
+  }
+ }
+ 
+ res.send(respuesta);
+});
 
-  app.post('/hola', function (req, res) {
-    res.send('Saludos desde post');
-  });   
-
+app.use(function(req, res, next) {
+ respuesta = {
+  error: true, 
+  codigo: 404, 
+  mensaje: 'URL no encontrada'
+ };
+ res.status(404).send(respuesta);
+});
 app.listen(3000, () => {
  console.log("El servidor está inicializado en el puerto 3000");
 });
